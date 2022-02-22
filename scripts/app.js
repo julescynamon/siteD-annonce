851d2f13a5e057a503ab263b9a41d12da5a61933
cynamon jules <julescweb@mail.com>
2022-02-22 12:13:18 +0100

    faq et appel a l'action

--- scripts/app.js
+++ scripts/app.js
@@ -24,3 +24,35 @@ btnNav.addEventListener('click', () => {
     }
 
 });
+
+
+// ------------- FAQ -------------
+
+const allCross = document.querySelectorAll ('.panneau-visible img');
+
+allCross.forEach(logo => {
+
+    logo.addEventListener('click', function() {
+
+        const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
+        const currentChoice = this.parentNode.parentNode.childNodes[3];
+
+        if (this.src.includes('plus')) {
+
+            this.src = '/ressources/minus.svg';
+            currentChoice.style.height = height + 40 + 'px';
+            currentChoice.style.opacity = 1;
+            currentChoice.style.padding = '20px 15px';
+
+        } else {
+
+            this.src = '/ressources/plus.svg';
+            currentChoice.style.height = 0;
+            currentChoice.style.opacity = 0;
+            currentChoice.style.padding = '0px 15px';
+
+        }
+
+    });
+
+});
