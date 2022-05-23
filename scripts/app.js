4393a4e504926e4c0e6ad7d3ef160778f1f7ca11
julescynamon <87892309+julescynamon@users.noreply.github.com>
2022-05-23 08:50:37 +0200

    maj

--- scripts/app.js
+++ scripts/app.js
@@ -34,19 +34,19 @@ allCross.forEach(logo => {
 
     logo.addEventListener('click', function () {
 
-        const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
-        const currentChoice = this.parentNode.parentNode.childNodes[3];
+        const height = this.parentNode.childNodes[3].scrollHeight;
+        const currentChoice = this.parentNode.childNodes[3];
 
         if (this.src.includes('plus')) {
 
-            this.src = '/ressources/minus.svg';
+            this.src = '../ressources/minus.svg';
             currentChoice.style.height = height + 40 + 'px';
             currentChoice.style.opacity = 1;
             currentChoice.style.padding = '20px 15px';
 
         } else {
 
-            this.src = '/ressources/plus.svg';
+            this.src = '../ressources/plus.svg';
             currentChoice.style.height = 0;
             currentChoice.style.opacity = 0;
             currentChoice.style.padding = '0px 15px';
